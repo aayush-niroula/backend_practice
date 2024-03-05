19ef0f90f3232a6eda0ff89938d5f6fcadd02c11
AAYUS KARKI <alexkarki2060@gmail.com>
2024-03-05 15:39:22 +0545

    DONE_pull_gar

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ const connectDB =async()=>{
     try {
         const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected ${connectionInstance.connection.host}`);
-        console.log(connectionInstance);
+        // console.log(connectionInstance);
     } catch (error) {
         console.log("MONGODB connection error",error);
         process.exit(1)        
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
-import { DB_NAME } from './constants';
+import { DB_NAME } from './constants.js';
 import connectDB from './db/index.js';
 
 dotenv.config({
     path:'./.env'
 })
 
-console.log(process.env.PORT);
+// console.log(process.env.PORT);
 
 connectDB()
 
